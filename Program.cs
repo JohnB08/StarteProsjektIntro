4226da50843228b7037aae88dcc9791c1790bd64
johnb08 <johnb08.kodehode@gmail.com>
2025-01-10 09:26:29 +0100

    tester readline metoden til Console

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("God Morgen!");
+var input = Console.ReadLine();
+Console.WriteLine($"You wrote: {input}");
